bfb181ff0011bb772c6153d6c070585ad72fc4ed
MN97490 <nathanalexandromichel@gmail.com>
2023-09-18 12:27:04 -0400

    Ajout connect.php style css et dossier img

---
+++ connect.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+    <link rel="stylesheet"  href="css/style.css">
+    <title>Connexion</title>
+</head>
+<body>
+
+
+    <div class="centering">
+            <div class="formulaireCo" >
+            
+                <form action=""method="post">
+                email: <input type="text" name="email" value = "" placeholder="email" ><br>
+                <p style="color:red;"></p>
+                password: <input type="text" name="password" value = "" placeholder="mot de passe"><br>
+                <p style="color:red;"></p>
+
+                <input type="submit" value="Connexion" >
+                </form> 
+            </div>
+
+    </div>
+   
+
+    
+</body>
+</html>
