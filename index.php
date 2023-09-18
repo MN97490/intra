1b796b71ab5f77e9df01964e1d99d5234a2310a5
jaulethug <xjeremiedrx@gmail.com>
2023-09-18 12:37:59 -0400

    ajout fichiers creation, index et vote et css

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <link rel="stylesheet"  href="css/style.css">
+
+    <title>index</title>
+</head>
+<body>
+    <h1 class="bienvenue">bienvenue sur la page index</h1>
+<button class="boutonjay"><a href="creation.php">creation.php</a></button>
+<button class="boutonjay"><a href="stat.php">stat.php</a></button>
+<button class="boutonjay"><a href="vote.php">vote.php</a></button>
+
+
+
+
+</body>
+</html>
